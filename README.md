565c0c67847d0e472286aa6261a28a17f0eb2c24
sharhan-alhassan <sharhanalhassan@gmail.com>
2021-10-19 16:53:15 +0000

    edited readme.md

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# pps-scanner
+# Portable Port Scanner -- ppscanner
 Portable Port Scanner (pps-scanner) is a light-weight open-source project CLI utility for showing services running on ports in a host 
+
+
+## Installation Guide
+
+```bash
+pip install -i https://test.pypi.org/simple/ hello-cli
+
+```
+
+## Run Commands
+```bash
+$ cooltool hello --name Sam
+```
+
+## For help commands
+```bash
+$ cooltool --help
+```
---
+++ setup.py
@@ -0,0 +1,31 @@
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+
+
+setup(
+    name = 'pps-scanner',
+    version = '0.0.1',
+    author = 'Sharhan Alhassan',
+    author_email = 'sharhanalhassan@gmail.com',
+    license = 'MIT',
+    description = 'A lightweight CLI tool for scanning ports and services on a host',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = 'https://github.com/sharhan-alhassan/pps-scanner',
+    py_modules = ['app'],
+    install_requires = [requirements],
+    python_requires ='>=3.7',
+    classifiers = [
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = '''
+        [console_scripts]
+        ppscan=app:cli
+    '''
+)
